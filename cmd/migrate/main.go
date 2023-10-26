5d3bb4148c728927f3074bd40e229635165c1f16
Gustavo Caso <gustavocaso@gmail.com>
2023-10-27 00:07:00 +0200

    Added goroutines and file creation

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -1,93 +1,146 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
-	"strings"
+	"os"
+	"path"
+	"path/filepath"
+	"sync"
+	"time"
 
 	"github.com/GustavoCaso/notion_workflows/pkg/utils"
 	"github.com/dstotijn/go-notion"
 )
 
 const (
-	pageID    = "7378e119-fc86-4e0c-b48c-922a14053dd4"
-	pageIDBis = "fd5f406e-72b2-4fc1-8b54-f8efc8d893e2"
+	dailyCheckDatabaseID = "3b27a5d9-138b-4f50-9c7b-7a77224f0579"
+	obsidianVault        = "/Users/gustavocaso/Documents/Obsidian Vault/Thoughts/Personal Notes"
 )
 
 func main() {
 	client := notion.NewClient(utils.GetAuthenticationToken())
+	filterTime, _ := time.Parse(time.RFC3339, "2020-01-01")
+	query := &notion.DatabaseQuery{
+		Filter: &notion.DatabaseQueryFilter{
+			Property: "Date",
+			DatabaseQueryPropertyFilter: notion.DatabaseQueryPropertyFilter{
+				Date: &notion.DatePropertyFilter{
+					After: &filterTime,
+				},
+			},
+		},
+		Sorts: []notion.DatabaseQuerySort{
+			{
+				Property:  "Date",
+				Direction: notion.SortDirAsc,
+			},
+		},
+	}
 
-	result, _ := client.FindBlockChildrenByID(context.Background(), pageIDBis, nil)
+	result, _ := client.QueryDatabase(context.Background(), dailyCheckDatabaseID, query)
+	wg := new(sync.WaitGroup)
 
-	// marshalled, _ := json.Marshal(result)
-	// fmt.Println(string(marshalled))
+	for _, result := range result.Results {
+		wg.Add(1)
+		properties := result.Properties.(notion.DatabasePageProperties)
+		date := properties["Date"].Date.Start
+		dayOfTheWeek := &properties["Day of the Week"].Formula.String
+		fileName := fmt.Sprintf("%s-%s.md", date.Format("2006-01-02"), **dayOfTheWeek)
+		obsidianPath := path.Join(obsidianVault, fmt.Sprint(date.Year()), fmt.Sprintf("%d-%s", int(date.Month()), date.Month().String()), fileName)
+		go fetchAndSaveToObsidianVault(wg, client, result.ID, obsidianPath)
+	}
 
-	var markdownResult strings.Builder
+	wg.Wait()
+}
 
-	for _, object := range result.Results {
+func fetchAndSaveToObsidianVault(wg *sync.WaitGroup, client *notion.Client, id, path string) {
+	defer wg.Done()
+	blocks, _ := client.FindBlockChildrenByID(context.Background(), id, nil)
+
+	if err := os.MkdirAll(filepath.Dir(path), 0770); err != nil {
+		panic(err)
+	}
+
+	f, err := os.Create(path)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	// create new buffer
+	buffer := bufio.NewWriter(f)
+
+	pageToMarkdown(client, blocks, buffer)
+}
+
+func pageToMarkdown(client *notion.Client, pageBlocks notion.BlockChildrenResponse, buffer *bufio.Writer) {
+	for _, object := range pageBlocks.Results {
 		switch block := object.(type) {
 		case *notion.Heading3Block:
-			markdownResult.WriteString("### ")
-			markdownResult.WriteString(writeRichText(client, block.RichText))
-			markdownResult.WriteString("\n")
-			markdownResult.WriteString("\n")
+			buffer.WriteString("### ")
+			writeRichText(client, buffer, block.RichText)
+			buffer.WriteString("\n")
+			buffer.WriteString("\n")
 		case *notion.ToDoBlock:
 			if *block.Checked {
-				markdownResult.WriteString("[x] ")
+				buffer.WriteString("[x] ")
 			} else {
-				markdownResult.WriteString("[ ] ")
+				buffer.WriteString("[ ] ")
 			}
-			markdownResult.WriteString(writeRichText(client, block.RichText))
-			markdownResult.WriteString("\n")
-			markdownResult.WriteString("\n")
+			writeRichText(client, buffer, block.RichText)
+			buffer.WriteString("\n")
+			buffer.WriteString("\n")
 		case *notion.ParagraphBlock:
 			if len(block.RichText) > 0 {
-				markdownResult.WriteString(writeRichText(client, block.RichText))
-				markdownResult.WriteString("\n")
-				markdownResult.WriteString("\n")
+				writeRichText(client, buffer, block.RichText)
+				buffer.WriteString("\n")
+				buffer.WriteString("\n")
 			} else {
-				markdownResult.WriteString("\n")
+				buffer.WriteString("\n")
 			}
 		case *notion.BulletedListItemBlock:
-			markdownResult.WriteString("- ")
-			markdownResult.WriteString(writeRichText(client, block.RichText))
-			markdownResult.WriteString("\n")
-			markdownResult.WriteString("\n")
+			buffer.WriteString("- ")
+			writeRichText(client, buffer, block.RichText)
+			buffer.WriteString("\n")
+			buffer.WriteString("\n")
 		case *notion.NumberedListItemBlock:
-			markdownResult.WriteString("- ")
-			markdownResult.WriteString(writeRichText(client, block.RichText))
-			markdownResult.WriteString("\n")
-			markdownResult.WriteString("\n")
+			buffer.WriteString("- ")
+			writeRichText(client, buffer, block.RichText)
+			buffer.WriteString("\n")
+			buffer.WriteString("\n")
 		case *notion.CalloutBlock:
-			markdownResult.WriteString("> [!")
+			buffer.WriteString("> [!")
 			if len(*block.Icon.Emoji) > 0 {
-				markdownResult.WriteString(*block.Icon.Emoji)
+				buffer.WriteString(*block.Icon.Emoji)
 			}
-			markdownResult.WriteString(writeRichText(client, block.RichText))
-			markdownResult.WriteString("]")
-			markdownResult.WriteString("\n")
-			markdownResult.WriteString("\n")
+			writeRichText(client, buffer, block.RichText)
+			buffer.WriteString("]")
+			buffer.WriteString("\n")
+			buffer.WriteString("\n")
 		default:
-			panic("type unknown")
+			errMessage := fmt.Sprintf("block not supported: %+v", block)
+			panic(errMessage)
 		}
 	}
 
-	fmt.Println("Result")
-	fmt.Print(markdownResult.String())
+	if err := buffer.Flush(); err != nil {
+		panic(err)
+	}
 }
 
-func writeRichText(client *notion.Client, richText []notion.RichText) string {
-	var result strings.Builder
-
+func writeRichText(client *notion.Client, buffer *bufio.Writer, richText []notion.RichText) {
 	for _, text := range richText {
 		switch text.Type {
 		case notion.RichTextTypeText:
 			if text.Annotations.Color == notion.ColorDefault {
-				result.WriteString(text.Text.Content)
+				buffer.WriteString(text.Text.Content)
 			} else {
-				result.WriteString("==")
-				result.WriteString(text.Text.Content)
-				result.WriteString("==")
+				buffer.WriteString("==")
+				buffer.WriteString(text.Text.Content)
+				buffer.WriteString("==")
 			}
 		case notion.RichTextTypeMention:
 			switch text.Mention.Type {
@@ -95,9 +148,13 @@ func writeRichText(client *notion.Client, richText []notion.RichText) string {
 				mentionPage, _ := client.FindPageByID(context.Background(), text.Mention.Page.ID)
 				if mentionPage.Parent.Type == notion.ParentTypeDatabase {
 					props := mentionPage.Properties.(notion.DatabasePageProperties)
-					result.WriteString(fmt.Sprintf("[[%s]]", writeRichText(client, props["Name"].Title)))
+					buffer.WriteString("[[")
+					writeRichText(client, buffer, props["Name"].Title)
+					buffer.WriteString("]]")
+				} else if mentionPage.Parent.Type == "" {
 				} else {
-					panic("mention page not supoprted")
+					errMessage := fmt.Sprintf("mention page not supported: %s", mentionPage.Parent.Type)
+					panic(errMessage)
 				}
 			default:
 				panic(fmt.Sprintf("mention type no supported: %s", text.Mention.Type))
@@ -106,6 +163,4 @@ func writeRichText(client *notion.Client, richText []notion.RichText) string {
 			panic("rich text equation not supported")
 		}
 	}
-
-	return result.String()
 }
